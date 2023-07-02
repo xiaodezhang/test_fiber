afad819b21cbf86a7379271b3e0945fc07512170
xiaodezhang <zhangxiaode0@gmail.com>
2023-07-02 15:24:46 +0800

    头部各显示调整

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { Canvas, useLoader } from '@react-three/fiber'
-import { useGLTF, useTexture, Decal, Environment, OrbitControls, RandomizedLight, AccumulativeShadows } from '@react-three/drei'
+import { useGLTF
+  , useTexture, Decal
+  , Text, RenderTexture, PerspectiveCamera
+  , Environment, OrbitControls, RandomizedLight, AccumulativeShadows } from '@react-three/drei'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { suspend } from 'suspend-react'
 
 export const App = () => (
   <Canvas shadows camera={{ position: [2, 2, 10], fov: 20 }}>
@@ -24,35 +28,345 @@ function Bun(props) {
   //   <mesh castShadow receiveShadow geometry={nodes.bunny.geometry} {...props} dispose={null}>
   //   </mesh>
   // )
+
   const { nodes, materials } = useGLTF('/scene-transformed.glb')
   return (
     <group {...props} dispose={null}>
       <mesh geometry={nodes.Object_4.geometry} material={materials.PaletteMaterial001} position={[0, 2.715, 0]} >
-      <Sticker url="/Sticjer_1024x1024@2x.png" position={[0.3, 0.5, 0.25]} rotation={Math.PI * 5.2} scale={0.45} />
+
+ <meshStandardMaterial color="black" roughness={0} metalness={0.5} />
+      <Decal position={[-0.13, -0.20, -0.75]} rotation={[0, Math.PI, 0]} scale={[0.23, 0.08, 1]}>
+        <meshStandardMaterial roughness={0.6} polygonOffset polygonOffsetFactor={-10}>
+          <RenderTexture attach="map" anisotropy={16}>
+            <PerspectiveCamera  makeDefault manual aspect={0.9 / 0.25} position={[0, 0, 5]} />
+            <color attach="background" args={['#af2040']} />
+            <ambientLight  intensity={0.5} />
+            <directionalLight  position={[10, 10, 5]} />
+            <Text  rotation={[0, 0, 0]} fontSize={5} color="white">
+                2000
+            </Text>
+          </RenderTexture>
+        </meshStandardMaterial>
+      </Decal>
+
+      <Decal position={[-0.13, -0.11, -0.75]} rotation={[0, Math.PI, 0]} scale={[0.23, 0.08, 1]}>
+        <meshStandardMaterial roughness={0.6} polygonOffset polygonOffsetFactor={-10}>
+          <RenderTexture attach="map" anisotropy={16}>
+            <PerspectiveCamera  makeDefault manual aspect={0.9 / 0.25} position={[0, 0, 5]} />
+            <color attach="background" args={['#af2040']} />
+            <ambientLight  intensity={0.5} />
+            <directionalLight  position={[10, 10, 5]} />
+            <Text  rotation={[0, 0, 0]} fontSize={5} color="white">
+                2000
+            </Text>
+          </RenderTexture>
+        </meshStandardMaterial>
+      </Decal>
+
+      <Decal position={[-0.39, -0.18, -0.75]} rotation={[0, Math.PI, 0]} scale={[0.23, 0.08, 1]}>
+        <meshStandardMaterial roughness={0.6} polygonOffset polygonOffsetFactor={-10}>
+          <RenderTexture attach="map" anisotropy={16}>
+            <PerspectiveCamera  makeDefault manual aspect={0.9 / 0.25} position={[0, 0, 5]} />
+            <color attach="background" args={['#af2040']} />
+            <ambientLight  intensity={0.5} />
+            <directionalLight  position={[10, 10, 5]} />
+            <Text  rotation={[0, 0, 0]} fontSize={5} color="white">
+                2000
+            </Text>
+          </RenderTexture>
+        </meshStandardMaterial>
+      </Decal>
+
+      <Decal position={[-0.39, -0.27, -0.75]} rotation={[0, Math.PI, 0]} scale={[0.23, 0.08, 1]}>
+        <meshStandardMaterial roughness={0.6} polygonOffset polygonOffsetFactor={-10}>
+          <RenderTexture attach="map" anisotropy={16}>
+            <PerspectiveCamera  makeDefault manual aspect={0.9 / 0.25} position={[0, 0, 5]} />
+            <color attach="background" args={['#af2040']} />
+            <ambientLight  intensity={0.5} />
+            <directionalLight  position={[10, 10, 5]} />
+            <Text  rotation={[0, 0, 0]} fontSize={5} color="white">
+                2000
+            </Text>
+          </RenderTexture>
+        </meshStandardMaterial>
+      </Decal>
+
+      <Decal position={[-0.39, -0.36, -0.75]} rotation={[0, Math.PI, 0]} scale={[0.23, 0.08, 1]}>
+        <meshStandardMaterial roughness={0.6} polygonOffset polygonOffsetFactor={-10}>
+          <RenderTexture attach="map" anisotropy={16}>
+            <PerspectiveCamera  makeDefault manual aspect={0.9 / 0.25} position={[0, 0, 5]} />
+            <color attach="background" args={['#af2040']} />
+            <ambientLight  intensity={0.5} />
+            <directionalLight  position={[10, 10, 5]} />
+            <Text  rotation={[0, 0, 0]} fontSize={5} color="white">
+                2000
+            </Text>
+          </RenderTexture>
+        </meshStandardMaterial>
+      </Decal>
+
+      <Decal position={[0.13, -0.18, -0.75]} rotation={[0, Math.PI, 0]} scale={[0.23, 0.08, 1]}>
+        <meshStandardMaterial roughness={0.6} polygonOffset polygonOffsetFactor={-10}>
+          <RenderTexture attach="map" anisotropy={16}>
+            <PerspectiveCamera  makeDefault manual aspect={0.9 / 0.25} position={[0, 0, 5]} />
+            <color attach="background" args={['#af2040']} />
+            <ambientLight  intensity={0.5} />
+            <directionalLight  position={[10, 10, 5]} />
+            <Text  rotation={[0, 0, 0]} fontSize={5} color="white">
+                2000
+            </Text>
+          </RenderTexture>
+        </meshStandardMaterial>
+      </Decal>
+
+      <Decal position={[0.13, -0.27, -0.75]} rotation={[0, Math.PI, 0]} scale={[0.23, 0.08, 1]}>
+        <meshStandardMaterial roughness={0.6} polygonOffset polygonOffsetFactor={-10}>
+          <RenderTexture attach="map" anisotropy={16}>
+            <PerspectiveCamera  makeDefault manual aspect={0.9 / 0.25} position={[0, 0, 5]} />
+            <color attach="background" args={['#af2040']} />
+            <ambientLight  intensity={0.5} />
+            <directionalLight  position={[10, 10, 5]} />
+            <Text  rotation={[0, 0, 0]} fontSize={5} color="white">
+                2000
+            </Text>
+          </RenderTexture>
+        </meshStandardMaterial>
+      </Decal>
+
+      <Decal position={[0.13, -0.36, -0.75]} rotation={[0, Math.PI, 0]} scale={[0.23, 0.08, 1]}>
+        <meshStandardMaterial roughness={0.6} polygonOffset polygonOffsetFactor={-10}>
+          <RenderTexture attach="map" anisotropy={16}>
+            <PerspectiveCamera  makeDefault manual aspect={0.9 / 0.25} position={[0, 0, 5]} />
+            <color attach="background" args={['#af2040']} />
+            <ambientLight  intensity={0.5} />
+            <directionalLight  position={[10, 10, 5]} />
+            <Text  rotation={[0, 0, 0]} fontSize={5} color="white">
+                2000
+            </Text>
+          </RenderTexture>
+        </meshStandardMaterial>
+      </Decal>
+
+      <Decal position={[-0.37, -0.20, 0.75]} rotation={[0, 0, 0]} scale={[0.23, 0.08, 1]}>
+        <meshStandardMaterial roughness={0.6} polygonOffset polygonOffsetFactor={-10}>
+          <RenderTexture attach="map" anisotropy={16}>
+            <PerspectiveCamera  makeDefault manual aspect={0.9 / 0.25} position={[0, 0, 5]} />
+            <color attach="background" args={['#af2040']} />
+            <ambientLight  intensity={0.5} />
+            <directionalLight  position={[10, 10, 5]} />
+            <Text  rotation={[0, 0, 0]} fontSize={5} color="white">
+                2000
+            </Text>
+          </RenderTexture>
+        </meshStandardMaterial>
+      </Decal>
+
+      <Decal position={[-0.37, -0.30, 0.75]} rotation={[0, 0, 0]} scale={[0.23, 0.08, 1]}>
+        <meshStandardMaterial roughness={0.6} polygonOffset polygonOffsetFactor={-10}>
+          <RenderTexture attach="map" anisotropy={16}>
+            <PerspectiveCamera  makeDefault manual aspect={0.9 / 0.25} position={[0, 0, 5]} />
+            <color attach="background" args={['#af2040']} />
+            <ambientLight  intensity={0.5} />
+            <directionalLight  position={[10, 10, 5]} />
+            <Text  rotation={[0, 0, 0]} fontSize={5} color="white">
+                2000
+            </Text>
+          </RenderTexture>
+        </meshStandardMaterial>
+      </Decal>
+
+      <Decal position={[-0.37, -0.40, 0.75]} rotation={[0, 0, 0]} scale={[0.23, 0.08, 1]}>
+        <meshStandardMaterial roughness={0.6} polygonOffset polygonOffsetFactor={-10}>
+          <RenderTexture attach="map" anisotropy={16}>
+            <PerspectiveCamera  makeDefault manual aspect={0.9 / 0.25} position={[0, 0, 5]} />
+            <color attach="background" args={['#af2040']} />
+            <ambientLight  intensity={0.5} />
+            <directionalLight  position={[10, 10, 5]} />
+            <Text  rotation={[0, 0, 0]} fontSize={5} color="white">
+                2000
+            </Text>
+          </RenderTexture>
+        </meshStandardMaterial>
+      </Decal>
+
+      <Decal position={[0.22, -0.20, 0.75]} rotation={[0, 0, 0]} scale={[0.23, 0.08, 1]}>
+        <meshStandardMaterial roughness={0.6} polygonOffset polygonOffsetFactor={-10}>
+          <RenderTexture attach="map" anisotropy={16}>
+            <PerspectiveCamera  makeDefault manual aspect={0.9 / 0.25} position={[0, 0, 5]} />
+            <color attach="background" args={['#af2040']} />
+            <ambientLight  intensity={0.5} />
+            <directionalLight  position={[10, 10, 5]} />
+            <Text  rotation={[0, 0, 0]} fontSize={5} color="white">
+                2000
+            </Text>
+          </RenderTexture>
+        </meshStandardMaterial>
+      </Decal>
+
+      <Decal position={[0.22, -0.30, 0.75]} rotation={[0, 0, 0]} scale={[0.23, 0.08, 1]}>
+        <meshStandardMaterial roughness={0.6} polygonOffset polygonOffsetFactor={-10}>
+          <RenderTexture attach="map" anisotropy={16}>
+            <PerspectiveCamera  makeDefault manual aspect={0.9 / 0.25} position={[0, 0, 5]} />
+            <color attach="background" args={['#af2040']} />
+            <ambientLight  intensity={0.5} />
+            <directionalLight  position={[10, 10, 5]} />
+            <Text  rotation={[0, 0, 0]} fontSize={5} color="white">
+                2000
+            </Text>
+          </RenderTexture>
+        </meshStandardMaterial>
+      </Decal>
+
+      <Decal position={[0.22, -0.40, 0.75]} rotation={[0, 0, 0]} scale={[0.23, 0.08, 1]}>
+        <meshStandardMaterial roughness={0.6} polygonOffset polygonOffsetFactor={-10}>
+          <RenderTexture attach="map" anisotropy={16}>
+            <PerspectiveCamera  makeDefault manual aspect={0.9 / 0.25} position={[0, 0, 5]} />
+            <color attach="background" args={['#af2040']} />
+            <ambientLight  intensity={0.5} />
+            <directionalLight  position={[10, 10, 5]} />
+            <Text  rotation={[0, 0, 0]} fontSize={5} color="white">
+                2000
+            </Text>
+          </RenderTexture>
+        </meshStandardMaterial>
+      </Decal>
+
+      <Decal position={[-0.08, -0.13, 0.75]} rotation={[0, 0, 0]} scale={[0.23, 0.08, 1]}>
+        <meshStandardMaterial roughness={0.6} polygonOffset polygonOffsetFactor={-10}>
+          <RenderTexture attach="map" anisotropy={16}>
+            <PerspectiveCamera  makeDefault manual aspect={0.9 / 0.25} position={[0, 0, 5]} />
+            <color attach="background" args={['#af2040']} />
+            <ambientLight  intensity={0.5} />
+            <directionalLight  position={[10, 10, 5]} />
+            <Text  rotation={[0, 0, 0]} fontSize={5} color="white">
+                2000
+            </Text>
+          </RenderTexture>
+        </meshStandardMaterial>
+      </Decal>
+      <Decal position={[-0.08, -0.22, 0.75]} rotation={[0, 0, 0]} scale={[0.23, 0.08, 1]}>
+        <meshStandardMaterial roughness={0.6} polygonOffset polygonOffsetFactor={-10}>
+          <RenderTexture attach="map" anisotropy={16}>
+            <PerspectiveCamera  makeDefault manual aspect={0.9 / 0.25} position={[0, 0, 5]} />
+            <color attach="background" args={['#af2040']} />
+            <ambientLight  intensity={0.5} />
+            <directionalLight  position={[10, 10, 5]} />
+            <Text  rotation={[0, 0, 0]} fontSize={5} color="white">
+                2000
+            </Text>
+          </RenderTexture>
+        </meshStandardMaterial>
+      </Decal>
+
+      <Decal  position={[-1, 0, 0.2]} rotation={[0, -Math.PI*0.5, 0]} scale={[0.15, 0.15, 1]}>
+        <meshStandardMaterial roughness={0.6} polygonOffset polygonOffsetFactor={-10}>
+          <RenderTexture attach="map" anisotropy={16}>
+            <PerspectiveCamera  makeDefault manual aspect={0.9 / 0.25} position={[0, 0, 5]} />
+            <color attach="background" args={['#af2040']} />
+            <ambientLight  intensity={0.5} />
+            <directionalLight  position={[10, 10, 5]} />
+            <Text  rotation={[0, 0, 0]} fontSize={4} color="white">
+                2000
+            </Text>
+          </RenderTexture>
+        </meshStandardMaterial>
+      </Decal>
+      <Decal position={[-1, 0, 0.0]} rotation={[0, -Math.PI*0.5, 0]} scale={[0.15, 0.15, 1]}>
+        <meshStandardMaterial roughness={0.6} polygonOffset polygonOffsetFactor={-10}>
+          <RenderTexture attach="map" anisotropy={16}>
+            <PerspectiveCamera  makeDefault manual aspect={0.9 / 0.25} position={[0, 0, 5]} />
+            <color attach="background" args={['#af2040']} />
+            <ambientLight  intensity={0.5} />
+            <directionalLight  position={[10, 10, 5]} />
+            <Text  rotation={[0, 0, 0]} fontSize={4} color="white">
+                2000
+            </Text>
+          </RenderTexture>
+        </meshStandardMaterial>
+      </Decal>
+      <Decal position={[-1, 0, -0.2]} rotation={[0, -Math.PI*0.5, 0]} scale={[0.15, 0.15, 1]}>
+        <meshStandardMaterial roughness={0.6} polygonOffset polygonOffsetFactor={-10}>
+          <RenderTexture attach="map" anisotropy={16}>
+            <PerspectiveCamera  makeDefault manual aspect={0.9 / 0.25} position={[0, 0, 5]} />
+            <color attach="background" args={['#af2040']} />
+            <ambientLight  intensity={0.5} />
+            <directionalLight  position={[10, 10, 5]} />
+            <Text  rotation={[0, 0, 0]} fontSize={4} color="white">
+                2000
+            </Text>
+          </RenderTexture>
+        </meshStandardMaterial>
+      </Decal>
+
+      <Decal position={[1, 0, 0.2]} rotation={[0, Math.PI*0.5, 0]} scale={[0.15, 0.15, 1]}>
+        <meshStandardMaterial roughness={0.6} polygonOffset polygonOffsetFactor={-10}>
+          <RenderTexture attach="map" anisotropy={16}>
+            <PerspectiveCamera  makeDefault manual aspect={0.9 / 0.25} position={[0, 0, 5]} />
+            <color attach="background" args={['#af2040']} />
+            <ambientLight  intensity={0.5} />
+            <directionalLight  position={[10, 10, 5]} />
+            <Text  rotation={[0, 0, 0]} fontSize={4} color="white">
+                2000
+            </Text>
+          </RenderTexture>
+        </meshStandardMaterial>
+      </Decal>
+      <Decal position={[1, 0, 0.0]} rotation={[0, Math.PI*0.5, 0]} scale={[0.15, 0.15, 1]}>
+        <meshStandardMaterial roughness={0.6} polygonOffset polygonOffsetFactor={-10}>
+          <RenderTexture attach="map" anisotropy={16}>
+            <PerspectiveCamera  makeDefault manual aspect={0.9 / 0.25} position={[0, 0, 5]} />
+            <color attach="background" args={['#af2040']} />
+            <ambientLight  intensity={0.5} />
+            <directionalLight  position={[10, 10, 5]} />
+            <Text  rotation={[0, 0, 0]} fontSize={4} color="white">
+                2000
+            </Text>
+          </RenderTexture>
+        </meshStandardMaterial>
+      </Decal>
+      <Decal position={[1, 0, -0.2]} rotation={[0, Math.PI*0.5, 0]} scale={[0.15, 0.15, 1]}>
+        <meshStandardMaterial roughness={0.6} polygonOffset polygonOffsetFactor={-10}>
+          <RenderTexture attach="map" anisotropy={16}>
+            <PerspectiveCamera  makeDefault manual aspect={0.9 / 0.25} position={[0, 0, 5]} />
+            <color attach="background" args={['#af2040']} />
+            <ambientLight  intensity={0.5} />
+            <directionalLight  position={[10, 10, 5]} />
+            <Text  rotation={[0, 0, 0]} fontSize={4} color="white">
+                2000
+            </Text>
+          </RenderTexture>
+        </meshStandardMaterial>
+      </Decal>
+
+      <Decal position={[1, -0.5, -0.1]} rotation={[0, Math.PI*0.5, 0]} scale={[0.15, 0.15, 1]}>
+        <meshStandardMaterial roughness={0.6} polygonOffset polygonOffsetFactor={-10}>
+          <RenderTexture attach="map" anisotropy={16}>
+            <PerspectiveCamera  makeDefault manual aspect={0.9 / 0.25} position={[0, 0, 5]} />
+            <color attach="background" args={['#af2040']} />
+            <ambientLight  intensity={0.5} />
+            <directionalLight  position={[10, 10, 5]} />
+            <Text  rotation={[0, 0, 0]} fontSize={4} color="white">
+                2000
+            </Text>
+          </RenderTexture>
+        </meshStandardMaterial>
+      </Decal>
+
+      <Decal position={[1, -0.5, 0.1]} rotation={[0, Math.PI*0.5, 0]} scale={[0.15, 0.15, 1]}>
+        <meshStandardMaterial roughness={0.6} polygonOffset polygonOffsetFactor={-10}>
+          <RenderTexture attach="map" anisotropy={16}>
+            <PerspectiveCamera  makeDefault manual aspect={0.9 / 0.25} position={[0, 0, 5]} />
+            <color attach="background" args={['#af2040']} />
+            <ambientLight  intensity={0.5} />
+            <directionalLight  position={[10, 10, 5]} />
+            <Text  rotation={[0, 0, 0]} fontSize={4} color="white">
+                2000
+            </Text>
+          </RenderTexture>
+        </meshStandardMaterial>
+      </Decal>
+
       </mesh>
     </group>
-  )
-}
-
-function Sticker({ url, ...props }) {
-  const emoji = useTexture(url)
-  return (
-    <Decal debug {...props}>
-      <meshPhysicalMaterial
-        transparent
-        polygonOffset
-        polygonOffsetFactor={-10}
-        map={emoji}
-        map-flipY={false}
-        map-anisotropy={16}
-        iridescence={1}
-        iridescenceIOR={1}
-        iridescenceThicknessRange={[0, 1400]}
-        roughness={1}
-        clearcoat={0.5}
-        metalness={0.75}
-        toneMapped={false}
-      />
-    </Decal>
   )
 }
